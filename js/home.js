00084bdd22c999c3b021b2464e094350e53ddc03
Shipra Sinha <shipra.juet@gmail.com>
2016-08-01 17:37:48 +0000

    links in head

--- js/home.js
+++ js/home.js
@@ -1,11 +1,3 @@
 $(document).ready(function(){
-	$('a').click(function(){ 
 
-		$('html, body').animate({ 
-			scrollTop: $( $.attr(this, 'href') ).offset().top 
-		}, 
-		500
-		); 
-		return false; });
-	new WOW().init();
 });
